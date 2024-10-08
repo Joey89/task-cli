5e86361aa7e27f9cb8d6ed242d0d54aae5b35e3d
Joey89 <under767@hotmail.com>
2024-10-08 08:39:34 -0400

    more updates

--- task-cli.py
+++ task-cli.py
@@ -8,19 +8,44 @@ taskList = {
     "updatedAt"     : "date and time last updated"
 }
 
-if len( sys.argv ) > 1 :
-    for arg in sys.argv:
-        if arg == 'add':
-            #create todo
-        elif arg == 'update':
-            pass
-        elif arg == 'delete':
-            pass
-        elif arg == 'list':
-            #seperate lists done, todo, in-progress
-            pass
-        
-# Convert the dictionary to a JSON string
-json_string = json.dumps(taskList)
 
-print(json_string)
+#need to have json file and looped over to see what next id will be.
+
+def todoList(args):
+    commandArg = args[1]
+
+    if commandArg == 'add':
+        task = args[1]
+        print('add')
+
+    elif commandArg == 'update':
+        if len(args) > 3:
+            taskID = args[2]
+            task =  args[3]
+            # task-cli update 1 "Buy groceries and cook dinner"
+            print('update')
+        else:
+            print('Requires 3 Arguments, ex: task-cli.py update 1 "todo"')
+            
+    elif commandArg == 'delete':
+        if len(args) > 2:
+            taskID = args[2] 
+            print('delete')
+        else:
+            print('Requires 2 Arguments, ex: task-cli.py delete 1')
+
+    elif commandArg == 'list':
+        if len(args) > 2:
+            listCommand = args[2]
+            print('Display list where listCommand is true')
+        else:
+            print('Display list')
+
+
+# Convert the dictionary to a JSON string
+##json_string = json.dumps(taskList)
+
+#print(json_string)
+
+if len( sys.argv ) > 1 :
+    todoList(sys.argv)
